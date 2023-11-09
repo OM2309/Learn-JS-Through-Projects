d29319b2a26ec4682ed2283ec9e6dbc831b83a1f
anurag <23anuragsharma@gmail.com>
2023-11-09 22:19:42 +0530

    Calculator

---
+++ Interview-ques/ques1.js
@@ -0,0 +1,23 @@
+// function sum (a,b){
+//     return a+b
+// }
+// console.log(sum(2,3))
+
+// function squareRoot(a){
+//     return a * a
+// }
+// console.log(squareRoot(2))
+
+// function triangle(base,height){
+//     return 1/2*(base*height)
+// }
+// console.log(triangle(2,3))
+
+let a = 5;
+let b = 6;
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log("a: " + a + " b: " + b)
